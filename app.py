5dd43b8e7df0cf8a6ec466e8baca198015bd18a2
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-07-02 20:19:22 +0300

    improve on how to obtain city name

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, jsonify
 import requests
+import ipinfo
 
 app = Flask(__name__)
 
-@app.route('/api/hello/<visitor_name>', methods=['GET'])
-def hello(visitor_name="Mark"):
-    client_ip = request.remote_addr
-    ipinfo_token = '3012b04ec95bc0'
-    location_response = requests.get(f'http://ipinfo.io/{client_ip}?token={ipinfo_token}')
-    location_data = location_response.json()
-    location = location_data.get('city', 'Huston')
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    visitor_name = request.args.get('visitor_name', 'Guest')
+    client_ip = str(request.remote_addr)
+    access_token = '3012b04ec95bc0'
+    handler = ipinfo.getHandler(access_token)
+    details = handler.getDetails()
+    city = details.city
     weather_api_key = '3a0d7a0f520144669f4737235ef6992f'
-    weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&APPID={weather_api_key}")
+    weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={weather_api_key}")
     weather_data = weather_response.json()
     temperature = weather_data['main']['temp']
 
     response = {
         'client_ip': client_ip,
-        'location': location,
-        'greeting': f'Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {location}',
+        'location': city,
+        'greeting': f'Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {city}',
     }
 
     return jsonify(response)
